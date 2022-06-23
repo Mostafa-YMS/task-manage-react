c069cb457bf04b46fbccd3fbcf0a1dc785940bdb
Mostafa-YMS <mos_302@yahoo.com>
2022-06-23 23:43:29 +0200

    task style

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,9 +1,26 @@
 const Task = ({ task }) => {
   return (
-    <>
+    <div
+      style={{
+        border: "2px solid #E2E2E2",
+        borderRadius: "8px",
+        padding: "10px",
+        margin: "10px",
+      }}
+    >
       <h5>{task?.title}</h5>
       <p>{task?.description}</p>
-    </>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <button>Delete</button>
+        <button>Edit</button>
+      </div>
+    </div>
   );
 };
 
